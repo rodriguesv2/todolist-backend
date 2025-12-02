ee7c670d94c81edb41222a3fd8aa60c38ffe9974
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2025-12-02 11:50:36 -0300

    Created the filter to check if the user is logged in

--- src/main/kotlin/br/com/rubensrodrigues/todolist/config/SecurityConfig.kt
+++ src/main/kotlin/br/com/rubensrodrigues/todolist/config/SecurityConfig.kt
@@ -1,14 +1,18 @@
 package br.com.rubensrodrigues.todolist.config
 
+import br.com.rubensrodrigues.todolist.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -17,8 +21,6 @@ class SecurityConfig {
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers("/users/**").permitAll()
-                    .requestMatchers("/todos/**").permitAll()
-                    .requestMatchers("/boards/**").permitAll()
                     .requestMatchers("/health/**").permitAll()
                     .requestMatchers("/auth/login").permitAll()
                     .requestMatchers("/h2-console/**").permitAll()
@@ -27,6 +29,7 @@ class SecurityConfig {
             .anonymous { it.disable() }
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         http.headers { headers ->
             headers.frameOptions { it.disable() }
